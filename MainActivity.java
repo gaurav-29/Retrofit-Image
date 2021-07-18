f7f2f89861795a6e2fc9a93c0495cb45c5b7d980
gaurav-29 <77089392+gaurav-29@users.noreply.github.com>
2021-07-18 20:36:59 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -6,9 +6,16 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import com.github.ybq.android.spinkit.sprite.Sprite;
+import com.github.ybq.android.spinkit.style.Circle;
 
 import java.util.ArrayList;
 
@@ -23,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     Context ctx = this;
     ArrayList<TestModel.Category> CategoryList = new ArrayList<>();
     CategoryAdapter mAdapter;
+    ProgressBar progressBar;
 
 
     @Override
@@ -30,8 +38,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        CheckInternetConnectivity();
         init();
+        ShowingProgressBar();
+        CallAPI();
+    }
 
+    private void CheckInternetConnectivity() {
+
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo wifiConn = cm.getNetworkInfo(cm.TYPE_WIFI);
+        NetworkInfo mobileConn = cm.getNetworkInfo(cm.TYPE_MOBILE);
+
+        if((wifiConn != null && wifiConn.isConnected()) || (mobileConn != null && mobileConn.isConnected())) {
+           Toast.makeText(ctx, "Connected to Internet", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(ctx, "Not Connected to Internet", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void ShowingProgressBar() {
+
+        Sprite circle = new Circle();
+        progressBar.setIndeterminateDrawable(circle);
+    }
+
+    private void CallAPI() {
 
         apiInterface = APIClient.getClient().create(APIInterface.class);
 
@@ -55,19 +89,22 @@ public class MainActivity extends AppCompatActivity {
                 recCategory.setLayoutManager(new GridLayoutManager(ctx,1));
                 recCategory.setItemAnimator(new DefaultItemAnimator());
                 recCategory.setAdapter(mAdapter);
+
+                progressBar.setVisibility(View.GONE);
             }
 
             @Override
             public void onFailure(Call<TestModel> call, Throwable t) {
 
                 Toast.makeText(ctx, t.getMessage(), Toast.LENGTH_LONG).show();
+                progressBar.setVisibility(View.GONE);
             }
         });
-
-
     }
+
     public void init()
     {
         recCategory = findViewById(R.id.recCategory);
+        progressBar = findViewById(R.id.spin_kit);
     }
 }
